c3f5d9296dee4a9db188ec9900fda0eddb5d6447
Roberto Carlos Jacinto Chirinos <rjacinto@belatrix.com>
2019-12-16 10:58:48 -0500

    adding code

--- src/create.js
+++ src/create.js
@@ -3,13 +3,22 @@ import { Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
-    const hello = Observable.create(function(observer) {
+    const hello = new Observable(observer => {
         observer.next('Hello');
         setTimeout(()=>{
             observer.next('World');
+            observer.complete();
         }, 2000);
+        
     });
     
-    const subscribe = hello.subscribe(evt => displayLog(evt));
+    const observer = {
+        next: evt => displayLog(evt),
+        error: err => console.err("[ERR] -",err),
+        complete: () => displayLog("[DONE]")
+    }
+    const subscribe = hello.subscribe(observer);
+    const subscribe2 = hello.subscribe(observer);
+    subscribe.unsubscribe();
     /** end coding */
 }
