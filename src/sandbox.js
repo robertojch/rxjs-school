1e230063a0a67324a6e178d371768614967e2c35
Roberto Carlos Jacinto Chirinos <rjacinto@belatrix.com>
2019-12-30 16:03:55 -0500

    adding code

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay } from './utils';
-import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { fromEvent, from } from 'rxjs';
+import { map, tap, pairwise } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -13,7 +13,13 @@ export default () => {
     //observable that returns scroll (from top) on scroll events
     const scroll$ = fromEvent(document, 'scroll').pipe(
         map(() => docElement.scrollTop),
-        tap(evt => console.log("[scroll]: ", evt))
+        tap(evt => console.log("[scroll]: ", evt)),
+        pairwise(),  // devuelve en un array el valor actual y previo  ejemplo [289,290]
+        tap(evt => console.log("after [scroll]: ", evt)),
+        tap(([previous, current]) => {
+            updateDisplay(current > previous ? 'DESC' : 'ASC');
+        }),
+        map(([previous, current]) => current)
     );
 
     //observable that returns the amount of page scroll progress
@@ -26,6 +32,13 @@ export default () => {
 
     //subscribe to scroll progress to paint a progress bar
     const subscription = scrollProgress$.subscribe(updateProgressBar);
+
+    const demof$ = from(['Hola', 'Roberto', 'Carlos', 'Jacinto', 'Chirinos']);
+    const demoSubscription = demof$.pipe(tap(evt => console.log('before tap evt', evt)),
+    pairwise()
+    ).subscribe(data => {
+        console.log('after pairwise ',data);
+    });
 
     /** end coding */
 }
